4d723e4035ef332cc2ca9a91b19f3ae67fc60f4c
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-10-30 12:53:48 -0300

    Create Model and DB for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_23_153218) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_30_135846) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -25,6 +25,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_23_153218) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
