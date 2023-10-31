4d27e733c248e8392e56b9eede2513ec098a5e75
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-10-31 13:15:52 -0300

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_30_135846) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_31_150815) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_30_135846) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
   add_foreign_key "comments", "articles"
